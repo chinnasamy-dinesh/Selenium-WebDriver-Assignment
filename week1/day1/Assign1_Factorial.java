0a7c1d91f16a353abc51350c2309213abfde22b7
dinesh-chinnasamy <115138922+chinnasamy-dinesh@users.noreply.github.com>
2022-10-15 09:51:52 +0100

    Add files via upload

---
+++ week1/day1/Assign1_Factorial.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Assign1_Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int myFact=10;
+		int fact = 1;
+		for (int i=1;i<=myFact;i++) 
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+
+}
