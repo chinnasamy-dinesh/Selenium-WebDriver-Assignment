2a4e04354ff98fc6ee5a853a416779e9834daaf0
dinesh-chinnasamy <115138922+chinnasamy-dinesh@users.noreply.github.com>
2022-11-12 07:14:48 +0000

    Add files via upload

---
+++ Day_2/Learn_While.java
@@ -0,0 +1,13 @@
+package week_1.Day_2;
+
+public class Learn_While {
+	
+	public static void main(String[] args) {
+		int input = 5;
+		while (input < 10) {
+			System.out.println(input);
+			input++;
+		}
+	}
+
+}
